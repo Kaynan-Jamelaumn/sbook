33197feb02b12c0d93238b83d33fbbdc3b43ef2e
Kaynan-Jamelaumn <kaynanrodrigues.nt@gmail.com>
2023-11-08 20:50:45 -0300

    I've forgotten to save the settings file xD

--- project/settings.py
+++ project/settings.py
@@ -141,3 +141,11 @@ REST_FRAMEWORK = {
 
 }
 AUTH_USER_MODEL = 'main.CustomUser'
+CORS_ALLOW_ALL_ORIGINS = True
+
+# CORS_ALLOWED_ORIGINS = [
+#     "https://example.com",
+#     "https://sub.example.com",
+#     "http://localhost:3000",
+#     "http://127.0.0.1:9000"
+# ]
